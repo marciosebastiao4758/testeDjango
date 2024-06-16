25f8e08b07c1d68daf1ecea929edd76edc28dea0
marciosebastiao4758 <marciosebastiao302626@hotmail.com>
2024-06-16 17:37:42 -0300

    criação do post_detail

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, post_detail
 
 urlpatterns = [    
-    path("", index),
+    path("", index, name="index"),
+     path("post_detail/<int:id>/", post_detail, name="post_detail"),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,7 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, "index.html", {"posts":posts})
+
+def post_detail(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, "post_detail.html", {"post":post})
