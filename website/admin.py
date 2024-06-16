d04537312fb67f6e3780d64964a51f5695a87e25
marciosebastiao4758 <marciosebastiao302626@hotmail.com>
2024-06-16 13:47:28 -0300

    criação da categorias de post

--- website/admin.py
+++ website/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["title", "sub_title"]
+    list_display = ["title", "sub_title", "categories"]
     search_fields = ['title', 'sub_title']
     
 
